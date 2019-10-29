37fee19099660d0f4833004c05b65ed64e937b58
RandomWP <l.vladik@outlook.com>
2019-10-30 02:01:11 +0300

    Finish core module

--- core.py
+++ core.py
@@ -1,12 +1,13 @@
 import utils
 from PIL.Image import Image
+from PIL.ImageDraw import Draw, ImageDraw
 
 
 class Lsystem(object):
-    def __init__(self, inititator: str, rules: dict = {}) -> None:
+    def __init__(self, name: str, inititator: str, rules: dict) -> None:
         self.initiator = inititator
         self.rules = rules
-        self.strings = None
+        self.strings = []
 
     def set_inititator(self, inititator: str) -> None:
         self.initiator = inititator
@@ -17,18 +18,24 @@ class Lsystem(object):
     def get_initiator(self) -> str:
         return self.initiator
 
-    async def generate_strings(self, iterations: int) -> None:
+    def get_strings(self):
+        pass
+
+    def get_string(self, iteration):
+        return self.strings[iteration]
+
+    def update_strings(self, iterations: int) -> None:
         strings = [self.initiator]
         for it in range(iterations):
-            strings.append(
-                map(lambda char: self.rules[char] if char in self.rules else char, strings[it]))
+            strings.append(str(
+                map(lambda char: self.rules[char] if char in self.rules else char, strings[it])))
 
 
 class LsystemImage(object):
     def __init__(self, lsystem: Lsystem,
                  size: tuple = (0, 0),
                  start_coords: tuple = (0, 0),
-                 start_angle: int = 0,
+                 start_angle: float = 0,
                  rot_angle: float = 0,
                  step_length: int = 1,
                  bg_color="#000000",
@@ -42,12 +49,46 @@ class LsystemImage(object):
         self.bg_color = bg_color
         self.line_color = line_color
 
-    def gen_lsystem_image(lsystem, size, bg_color, draw_params):
-        img = Image.new("RGBA", size, bg_color)
-        dr = Draw(img)
-        draw_lsystem(dr, lsystem, *draw_params, size)
-        del dr
-        return img
+    def _make_empty_image(self) -> None:
+        self.image = Image.new("RGBA", self.size, self.bg_color)
 
-    def generate_image(self, iteration: int) -> Image:
-        pass
+    def _make_draw(self) -> None:
+        self.draw = Draw(self.image)
+
+    def _draw_lsystem(self) -> None:
+        not_drawn_lines = 0
+        x, y = self.start_coords[0], self.start_coords[1]
+        angle = self.start_angle
+        stack = []
+        for char in lsystem:
+            if char == "F":
+                new_x, new_y = point_on_circle((x, y), step_length, angle)
+                if check_coords((x, y), (new_x, new_y), max_coords):
+                    self.draw.line(((x, y), (new_x, new_y)),
+                                   fill=line_color, width=1)
+                    not_drawn_lines = 0
+                else:
+                    not_drawn_lines += 1
+                x, y = new_x, new_y
+            elif char == "f":
+                x, y = point_on_circle((x, y), step_length, angle)
+            elif char == "+":
+                angle += self.rotation_angle
+            elif char == "-":
+                angle -= self.rotation_angle
+            elif char == "|":
+                angle += pi
+            elif char == "[":
+                stack.append((x, y, angle))
+            elif char == "]":
+                x, y, angle = stack.pop()
+            if not_drawn_lines >= 30000:
+                break
+
+    def get_image(self) -> Image:
+        return self.image
+
+    def update_image(self, iteration: int) -> None:
+        self._make_empty_image()
+        self._make_draw()
+        self._draw_lsystem()
